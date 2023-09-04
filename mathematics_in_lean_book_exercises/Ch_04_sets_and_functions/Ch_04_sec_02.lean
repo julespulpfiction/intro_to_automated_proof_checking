27f2a8e1793a763d58b582cdacd3915e6334006e
Sumant Chopde <108463143+sumantchopde@users.noreply.github.com>
2023-09-05 00:42:52 +0530

    Update Ch_04_sec_02.lean

--- mathematics_in_lean_book_exercises/Ch_04_sets_and_functions/Ch_04_sec_02.lean
+++ mathematics_in_lean_book_exercises/Ch_04_sets_and_functions/Ch_04_sec_02.lean
@@ -1,3 +1,15 @@
+import Mathlib.Tactic
+import Mathlib.Data.Set.Lattice
+import Mathlib.Data.Set.Function
+import Mathlib.Analysis.SpecialFunctions.Log.Basic
+
+section
+
+variable {α β : Type _}
+variable (f : α → β)
+variable (s t : Set α)
+variable (u v : Set β)
+
 example : f ¹' (u ∩ v) = f ¹' u ∩ f ¹' v := by
-ext
-rfl
+  ext
+  rfl
