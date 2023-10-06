999f9493f5027edefb4d6e48f7d8fe51f26d1a81
sumantchopde <chopde.sumant@students.iiserpune.ac.in>
2023-10-06 20:51:36 +0530

    Create Ch_06_sec_01.lean

---
+++ mathematics_in_lean_book_exercises/Ch_06_mathematical_structures/Ch_06_sec_01.lean
@@ -0,0 +1,18 @@
+import Mathlib.Tactic
+import Mathlib.Algebra.BigOperators.Ring
+import Mathlib.Data.Real.Basic
+
+namespace C06S01
+noncomputable section
+
+@[ext]
+structure Point where
+  x : ℝ
+  y : ℝ
+  z : ℝ
+
+def add (a b : Point) : Point :=
+  ⟨a.x + b.x, a.y + b.y, a.z + b.z⟩
+
+protected theorem add_assoc (a b c : Point) : (a.add b).add c = a.add (b.add c) := by
+  sorry
