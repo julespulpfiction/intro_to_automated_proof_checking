dbdf1ea9fe8936cf32cc336a6246319ba5f2cdae
sumantchopde <chopde.sumant@students.iiserpune.ac.in>
2023-10-06 20:56:06 +0530

    Update Ch_06_sec_01.lean

--- mathematics_in_lean_book_exercises/Ch_06_mathematical_structures/Ch_06_sec_01.lean
+++ mathematics_in_lean_book_exercises/Ch_06_mathematical_structures/Ch_06_sec_01.lean
@@ -11,8 +11,12 @@ structure Point where
   y : ℝ
   z : ℝ
 
+namespace Point
+
 def add (a b : Point) : Point :=
   ⟨a.x + b.x, a.y + b.y, a.z + b.z⟩
 
 protected theorem add_assoc (a b c : Point) : (a.add b).add c = a.add (b.add c) := by
   sorry
+
+end Point
