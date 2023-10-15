bd9bf1807f398400ad1657cd28d8a463d05c2278
sumantchopde <chopde.sumant@students.iiserpune.ac.in>
2023-10-16 01:17:30 +0530

    Updated Ch_08_sec_01.lean

--- mathematics_in_lean_book_exercises/Ch_08_topology/Ch_08_sec_01.lean
+++ mathematics_in_lean_book_exercises/Ch_08_topology/Ch_08_sec_01.lean
@@ -10,6 +10,42 @@ def principal {α : Type _} (s : Set α) : Filter α
   -- 1. The set of all elements of α belongs to the filter
   -- 2. If A is in the filter and A ⊆ B, then B is in the filter.
   -- 3. If A and B are in the filter, then so is A ∩ B.
-  univ_sets := sorry
-  sets_of_superset := sorry
-  inter_sets := sorry
+  univ_sets := subset_univ s
+  sets_of_superset := fun hU hUV ↦ Subset.trans hU hUV
+  inter_sets := fun hU hV ↦ subset_inter hU hV
+
+example : Filter ℕ :=
+  { sets := { s | ∃ a, ∀ b, a ≤ b → b ∈ s }
+    univ_sets := by
+      use 
+      simp
+
+    sets_of_superset := by
+      rintro U V ⟨N, hN⟩ hUV
+      use N
+      tauto
+      
+    inter_sets := by
+      rintro U V ⟨N, hN⟩ ⟨N', hN'⟩
+      use max N N'
+      intro b hb
+      rw [max_le_iff] at hb
+      constructor <;> tauto }
+
+def Tendsto₁ {X Y : Type _} (f : X → Y) (F : Filter X) (G : Filter Y) :=
+  ∀ V ∈ G, f ⁻¹' V ∈ F
+
+example {X Y Z : Type _} {F : Filter X} {G : Filter Y} {H : Filter Z} {f : X → Y} {g : Y → Z}
+    (hf : Tendsto₁ f F G) (hg : Tendsto₁ g G H) : Tendsto₁ (g ∘ f) F H :=
+  calc
+    map (g ∘ f) F = map g (map f F) := by rw [map_map]
+    _ ≤ map g G := (map_mono hf)
+    _ ≤ H := hg
+
+example {X Y Z : Type _} {F : Filter X} {G : Filter Y} {H : Filter Z} {f : X → Y} {g : Y → Z}
+    (hf : Tendsto₁ f F G) (hg : Tendsto₁ g G H) : Tendsto₁ (g ∘ f) F H := by
+  intro V hV
+  rw [preimage_comp]
+  apply hf
+  apply hg
+  exact hV
---
+++ mathematics_in_lean_book_exercises/Ch_08_topology/test.lean
@@ -0,0 +1,6 @@
+/-
+unknown package 'Mathlib'
+-/
+import Mathlib.Tactic
+
+#check 2 + 2
